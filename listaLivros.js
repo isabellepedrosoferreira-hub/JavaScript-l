935e352a450e601ffe58ffae5f3c9f97a079d9a2
isabellepedrosoferreira-hub <isabelle.pedroso.ferreira@escola.pr.gov.br>
2026-05-06 19:12:31 +0000

    atividade finalizada

---
+++ listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+{ 
+    titulo: "Extraordinário", 
+    preco: 50
+},
+{ 
+    titulo: "É Assim que Acaba", 
+    preco: 36 
+},
+{ 
+    titulo: "O Pequeno Principe", 
+    preco: 40
+},
+{ 
+    titulo: "Verity", 
+    preco: 25
+},
+{ 
+    titulo: "Vidas Secas", 
+    preco: 20
+},
+{ 
+    titulo: "O Poder do Hábito", 
+    preco: 10
+}
+];
+
+module.exports = precosLivros;
